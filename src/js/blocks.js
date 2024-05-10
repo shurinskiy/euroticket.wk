3af931b59c50de7fcd8671bcc5bfb0b3e5c79a2b
_tango <shurinskiy@mail.ru>
2024-05-10 12:34:48 +0500

    add marquee on mainpage

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -2,3 +2,4 @@ import "../blocks/header/header.js";
 import "../blocks/select/select.js";
 import "../blocks/upward/upward.js";
 import "../blocks/gallery/gallery.js";
+import "../blocks/marquee/marquee.js";
